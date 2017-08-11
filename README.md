4d549e232902b6b51619bf8ca7189d20b4f722c6
Miguel <santosmiguel25@gmail.com>
2017-08-11 21:07:21 +0100

    Added python test script

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ ___
 It is an intermediate language that compiles to either Batch or Shell and is capable of running its own scripts in a "JIT" fashion without having to compile to either language (which are platform dependent).
 
 Also, it will be possible to translate the intermediate language into any other scripting language, for example Powershell.
+___
+#How to run
+For now, we can only run scripts as tests.  
+
+To run them, simply type on the command line:  
+
+`python test.pyc ex1.sl`
+
+Make sure your current working directory is `<top>/test`.
--- src/main/Scriptlang.java
+++ src/main/Scriptlang.java
@@ -1,8 +1,50 @@
 package main;
 
+import com.parser.ScriptlangParser;
+
+class Dbg {
+	private static boolean enabled = false;
+	private final static String promptPrefix = "Scriptlang";
+	
+	public static void setDebugging(boolean enableDebugging) {
+		enabled = enableDebugging;
+	}
+	
+	public static boolean log(Object o) {
+		if(!enabled)
+			return false;
+		System.out.println(promptPrefix + "> " + o.toString());
+		return true;
+	}
+}
+
 public class Scriptlang {
+	public static void help() {
+		Dbg.setDebugging(true);
+		Dbg.log("HELP\nUsage: java main.Scriptlang <testfile>\nExample: java main.Scriptlang ../test/ex1.sl\n\n(Note: Current working directory must be <PROJECTFOLDER>/bin)");
+	}	
+
+	@SuppressWarnings("static-access")
 	public static void main(String[] args) throws Exception {
-		System.out.println("Scriptlang: Parsing...");
-		com.parser.ScriptlangParser.main(args);
+		/* Check command line arguments */
+		if(args.length < 1) {
+			help();
+			System.exit(1);
+		}
+		
+		/* Enable debugging messages */
+		Dbg.setDebugging(true);
+		Dbg.log("Parsing '"+ args[0] +"'...");
+		
+		/* Instantiate parser */
+		ScriptlangParser parser = new ScriptlangParser(new java.io.FileInputStream(args[0]));
+		
+		/* Parse input file */
+		parser.Input();
+		
+		/* All done */
+		Dbg.log("Done parsing '"+ args[0] +"'.");
+		
+		System.exit(0);
 	}
 }
